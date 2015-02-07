92da57df1cde51fad89874435897749645a91a95
Fabien Basmaison <fabien@arkhi.org>
2015-02-07 14:32:02 +0800

    [Chapter 1] UX:
    * Display error and hide interface if no JS.
    * Move JS from html to js.

--- assignment-01/assets/js/all/diary.js
+++ assignment-01/assets/js/all/diary.js
@@ -1,10 +1,9 @@
 (function (exports, $) {
     // Display an error message and stop the script if localStorage is not available.
     if ('undefined' === typeof(localStorage)) {
-        $("#posts").append($("<div class='notification warning.'>Sorry! This application won’t " +
+        $("#posts").append($("<div class='notification error.'>Sorry! This application won’t " +
                              "work because your browser doesn’t support localStorage. :|</div>"));
         $('#add-entry').remove();
-        console.log('hello');
         return exports;
     }
 
@@ -43,6 +42,7 @@
     exports.addTxt = function () {
         $("#add-text").removeClass('is-hidden').find("input").focus();
     };
+
     exports.okEdit = function () {
         var subject = $("#add-text input").val();
         if (!subject) {
@@ -58,11 +58,46 @@
         exports.cancelEdit();
         showEntries();
     };
+
     exports.cancelEdit = function () {
         $("#add-text input").val("");
         $("#add-text textarea").val("");
         $("#add-text").addClass('is-hidden')();
     };
-    
+
+    /**
+     * Since China is blocking anything related to Google, avoid blocking the loading process if
+     * the user doesn’t have a VPN. Lazyload.css is here to avoid cross-domain problems; its content
+     * is http://fonts.googleapis.com/css?family=Over+the+Rainbow%7CLobster%7CBangers%7CJulee.
+     *
+     * TODO: Update with location based detection and an object with country:[domains]?
+     * TODO: Check http://www.w3.org/TR/html5/document-metadata.html#attr-link-crossorigin and
+     *       http://blogs.telerik.com/kendoui/posts/11-10-03/using_cors_with_all_modern_browsers.
+     */
+    exports.loadFonts = function () {
+        var lazyLoad = {
+            url: 'css/lazyload.min.css',
+            media: 'all and (min-width:40em)'
+        };
+        if (document.createStyleSheet) {
+            document.createStyleSheet(lazyLoad.url).setAttribute('media', lazyLoad.media);
+        } else {
+            $('<link/>', {
+                rel:   'stylesheet',
+                href:  lazyLoad.url,
+                media: lazyLoad.media,
+            }).insertBefore('#loadLast');
+        }
+    }
+
+    /**
+     * If the browser can use JS, show interface.
+     */
+    function hasJS () {
+        $("#sidebar").removeClass('is-hidden');
+    }
+
+    $(hasJS);
+    $(loadFonts);
     $(showEntries);
 })(window, jQuery);
